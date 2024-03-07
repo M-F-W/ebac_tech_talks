dcf0c9ed81dd22c48b870ecde13c0c7eb0875cb9
Matheus de Freitas Weber <matheusfweber@hotmail.com>
2024-03-07 16:06:54 -0300

    Criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-console.log("olá mundo!");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const segundosEmMs = 1000;
+    const minutosEmMs = segundosEmMs * 60;
+    const horasEmMs = minutosEmMs * 60;
+    const diasEmMs = horasEmMs * 24;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor(
+        (distanciaAteOEvento % minutosEmMs) / segundosEmMs
+    );
+
+    document.getElementById(
+        "contador"
+    ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        document.getElementById("contador").innerHTML = "Evento Expirado";
+    }
+}, 1000);
